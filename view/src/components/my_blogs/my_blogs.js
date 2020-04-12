7a2e2053cda8257a6617743dcab91f1d1689a04d
riyaagrahari <riyaagrahari121@gmail.com>
2020-04-13 03:03:01 +0530

    Added components

---
+++ view/src/components/my_blogs/my_blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyBlogs = () => {
+        return(
+        <div className="container">
+            myblogs
+        </div>
+        );
+}
+
+export default MyBlogs;
