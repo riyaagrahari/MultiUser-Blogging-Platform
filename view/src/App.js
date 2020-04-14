427d41ad437db360f664177ced9f950aa1307e4f
riyaagrahari <riyaagrahari121@gmail.com>
2020-04-15 01:16:03 +0530

    Moved navigation routes to App.js

--- view/src/App.js
+++ view/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import Home from './components/home/home'
-import Navigation from './components/navigation/navigation';
+import { Router } from '@reach/router'
+import Profile from './components/profile/profile.js'
+import Home from './components/home/home.js'
+import MyBlogs from './components/my_blogs/my_blogs.js'
 
 function App() {
+  const Navigation = () => {
+    const navLinks = [
+        {
+          text: 'Home',
+          path: '/',
+          icon: 'ios-ios-megaphone'
+        },
+        {
+            text: 'Profile',
+            path: '/profile',
+            icon: 'ios-ios-megaphone'
+        },        
+        {
+            text: 'MyBlogs',
+            path: '/myblogs',
+            icon: 'ios-ios-megaphone'
+        }        
+    ]
+  }
   return (
-    <div className="App">
-      <Navigation />
+    <div className="Nav">
+            <Router>
+                <Home path="/" />
+                <Profile path="/profile" />
+                <MyBlogs path="/myblogs" />
+            </Router>
     </div>
   );
 }
