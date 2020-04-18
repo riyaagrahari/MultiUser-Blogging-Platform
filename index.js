49ed715fe8bd3669358632bfdb041ef8d578e5f4
Nikhil-Anand <anandnikhil91@gmail.com>
2020-04-18 18:43:48 +0530

    functions for insert and find

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const insertNewUser = require('./model/addUser');
 
 const corsConfig = {
     origin: true,
@@ -10,11 +9,22 @@ const corsConfig = {
 };
 
 app.use(cors(corsConfig));
-const newUser = insertNewUser()
-    .then(r => {
-        console.log("Addition successful", r)
-    })
-    .catch(err => {
-        console.log(err);
-    })
+
 app.listen(8008, () => console.log(`Started listening on port 8008!`));
+
+// const insertNewUser = require('./model/addUser');
+// const newUser = insertNewUser()
+//     .then(r => {
+//         console.log("Addition successful", r)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+// const searchUser = require('./model/searchUser');
+// const searchUserRun = searchUser()
+//     .then(r => {
+//         console.log("Successfully Found: ", r)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
